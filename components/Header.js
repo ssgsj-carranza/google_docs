7660a63bf86de00c2880d2cd0bee4e193e46de98
unknown <ssgsj.carranza@gmail.com>
2021-09-20 19:49:46 -0500

    pulled in search icon, added and styled input field

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,9 @@
-import Button from '@material-tailwind/react/Button';
+import Button from "@material-tailwind/react/Button";
 import Icon from '@material-tailwind/react/Icon';
 
 function Header() {
     return (
-        <div className="flex items-center">
+        <div className="flex items-center sticky top-0 z-50 px-4 py-2 shadow-md bg-white">
             <Button
                 color='gray'
                 buttonType='outline'
@@ -12,10 +12,14 @@ function Header() {
                 ripple='dark'
                 className='h-20 w-20 border-0'
             >
-                <Icon name='menu' swize='3xl'/>
+                <Icon name='menu' size='3xl'/>
             </Button>
             <Icon name='description' size='5xl' color='blue' />
             <h1 className='hidden md:inline-flex ml-2 text-gray-700 text-2xl'>Docs</h1>
+            <div className='flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg'>
+                <Icon name='search' size='3xl' color='gray' />
+                <input type="text" placeholder='Search' className='flex-grow px-5 text-base bg-transparent outline-none'/>
+            </div>
         </div>
     )
 }
