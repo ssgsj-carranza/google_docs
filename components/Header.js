a22f744eb6005363da7b67e95021b6faf0cc1789
unknown <ssgsj.carranza@gmail.com>
2021-09-20 19:16:05 -0500

    pulled in and styled button with menu icon, working onb resolving issue where button isnt rendering on screen

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,16 @@ import Icon from '@material-tailwind/react/Icon';
 function Header() {
     return (
         <div className="flex items-center">
-
+            <Button
+                color='gray'
+                buttonType='outline'
+                rounded={true}
+                iconOnly={true}
+                ripple='dark'
+                className='hidden md:inline-flex h-20 w-20 border-0'
+            >
+                <Icon name='menu' swize='3xl'/>
+            </Button>
         </div>
     )
 }
