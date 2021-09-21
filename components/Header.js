dfa32ddfcc727cd818609f687a6ecc9818a9fbbe
unknown <ssgsj.carranza@gmail.com>
2021-09-20 19:11:55 -0500

    installed material tailwind, and used proper imports per documentation to pull in icons into header component, wrapped app js return statement in fragment to avoid error

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,16 @@
+import Button from '@material-tailwind/react/Button';
+import Icon from '@material-tailwind/react/Icon';
+
 function Header() {
     return (
-        <div>
-            
+        <div className="flex items-center">
+
         </div>
     )
 }
 
 export default Header
+
+
+//LIBRARIES
+// npm install -E @material-tailwind/react
