010657e4fe59354cfcec63c7926c53cdeea5d2a4
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-09-22 21:05:53 -0500

    finished setting up firebase per documentation, however firebase doesnt seem to have been installed correctly, working on solving issue

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCuN-lTnkd27TqxSchQg2N4_hY5m5-TImE",
+    authDomain: "docs-ec9be.firebaseapp.com",
+    projectId: "docs-ec9be",
+    storageBucket: "docs-ec9be.appspot.com",
+    messagingSenderId: "522262544282",
+    appId: "1:522262544282:web:650b8ae82fce6f441ac423"
+  };
+
+  const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
+
+  const db = app.firestore();
+
+  export {db};
