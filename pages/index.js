4f27e1e594797c614cfc5cb39618982a215250c4
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-09-22 20:42:40 -0500

    finished up styling display document section

--- pages/index.js
+++ pages/index.js
@@ -42,11 +42,11 @@ export default function Home() {
       </section>
 
     {/* DISPLAY DOCUMENTS SECTION */}
-      <section>
-        <div className='max-w-3xl mx-auto py-8'> 
-          <div>
-            <h2>My Documents</h2>
-            <p>Date Created</p>
+      <section className='bg-white px-10 md:px-0'>
+        <div className='max-w-3xl mx-auto py-8 text-sm text-gray-700'> 
+          <div className='flex items-center justify-between pb-5'>
+            <h2 className='font-medium flex-grow'>My Documents</h2>
+            <p className='mr-12'>Date Created</p>
             <Icon name='folder' size='3xl' color='gray'/>
           </div>
         </div>
