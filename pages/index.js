30f5b39524007cbc414ecc91b6bd9f4b8f07cbb4
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-09-22 20:20:22 -0500

    added some styling to template section, pulled in vert icon and added styling to it as well

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Button from '@material-tailwind/react/Button'
+import Icon from '@material-tailwind/react/Icon'
 import Head from 'next/head'
 import Header from '../components/Header'
 
@@ -9,9 +11,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <section>
+      <section className="bg-[#F8F9FA] pb-10 px-10">
         <div className="max-w-3xl mx-auto">
-          
+          <div className="py-6 flex items-center justify-between">
+            <h2 className="text-gray-700 text-lg">Start a new document</h2>
+            <Button 
+              color='gray'
+              buttonType='outline'
+              iconOnly={true}
+              ripple='dark'
+              className="border-0"
+            >
+              <Icon name='more_vert' size="3xl" />
+            </Button>
+          </div>
         </div>
       </section>
     </div>
