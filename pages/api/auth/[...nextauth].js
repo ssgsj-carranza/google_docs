05deefdf59f13aa248ab67e700e0cceb17989d3e
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-09-29 18:37:14 -0500

    fixed authentication error, forgot to add secret key and client id key to env local file, changed process.env variable names as well

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,8 +7,8 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
     // ...add more providers here
   ],
