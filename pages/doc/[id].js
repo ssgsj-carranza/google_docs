501b7e642f62ac9b9d9e27850e1bf3aa059872fc
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-09-30 19:30:24 -0500

    pulled in icon with a bit of styling for the header portion on the script page, added router to the icon to direct user back to the homescreen

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -1,9 +1,27 @@
+// import TextEditor from '../../components/TextEditor';
+import Button from '@material-tailwind/react/Button';
+import Icon from '@material-tailwind/react/Icon';
+import {useRouter} from 'next/dist/client/router';
+import {db} from '../../firebase';
+import {useDocumentOnce} from 'react-firebase-hooks/firestore';
+import {getSession, signOut, useSession} from 'next-auth/client';
+import Login from '../../components/Login';
+
 function Doc() {
+    const [session] = useSession();
+    const router = useRouter();
+
+    if(!session) return <Login />;
+    
     return (
         <div>
-            <h1>Text</h1>
+            <header className="flex justify-between items-center p-3 pb-1">
+                <span onClick={() => router.push('/')}>
+                    <Icon name="description" size="5xl" color='blue'/>
+                </span>
+            </header>
         </div>
-    )
+    );
 }
 
-export default Doc
+export default Doc;
