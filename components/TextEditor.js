3faf426634d8c3dd79fb40584d94d91ce8861180
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-10-04 20:49:06 -0500

    created editor state, pulled in create empty function from editor state import, added on change event to keep track ofeditor state, tested it and it works

--- components/TextEditor.js
+++ components/TextEditor.js
@@ -1,5 +1,7 @@
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import dynamic from 'next/dynamic';
+import { useState } from 'react';
+import {EditorState} from 'draft-js';
 
 //Use dynamic import to render editor on the client side not on the node js side, to avoid window is not define error
 const Editor = dynamic(() => import ('react-draft-wysiwyg').then(
@@ -10,9 +12,17 @@ const Editor = dynamic(() => import ('react-draft-wysiwyg').then(
 );
 
 function TextEditor() {
+    const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+    const onEditorStateChange  = (editorState) => {
+        setEditorState(editorState);
+    };
+
     return (
         <div className="bg-[#F8F9FA] min-h-screen pb-16">
-            <Editor 
+            <Editor
+                editorState={editorState}
+                onEditorStateChange={onEditorStateChange} 
                 toolbarClassName="flex sticky top-0 z-50 !justify-center mx-auto"
                 editorClassName="mt-6 bg-white shadow-lg max-w-5xl mx-auto mb-12 border p-10"
             />
