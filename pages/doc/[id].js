a3f8d9079b80dae8eaad5f326705710574184a51
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-10-04 20:15:22 -0500

    pulled in and styled share button with icon within, working on pulling user image into script header

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -33,7 +33,7 @@ function Doc() {
                     <Icon name="description" size="5xl" color='blue'/>
                 </span>
                 <div className="flex-grow px-2">
-                    <h2>{snapshot?.data()?.fileName}</h2>
+                    <h2 className='font-semibold'>{snapshot?.data()?.fileName}</h2>
                     <div className='flex items-center text-sm space-x-1 -ml-1 h-8 text-gray-600'>
                         <p className='option'>File</p>
                         <p className='option'>Edit</p>
@@ -43,6 +43,18 @@ function Doc() {
                         <p className='option'>Tools</p>
                     </div>
                 </div>
+                <Button
+                    color='lightBlue'
+                    buttonType='filled'
+                    size='regular'
+                    className='hidden md:inline-flex h-10'
+                    rounded={false}
+                    block={false}
+                    iconOnly={false}
+                    ripple='light'
+                >
+                    <Icon name='people' size='md'/>SHARE
+                </Button>
             </header>
         </div>
     );
